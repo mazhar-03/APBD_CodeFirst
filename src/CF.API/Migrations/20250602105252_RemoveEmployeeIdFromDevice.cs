df062bd1ef27517296991d91af35e57df17ed48d
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-06-02 14:58:07 +0200

    since had a mistake on coding the models at first, had to do that

---
+++ src/CF.API/Migrations/20250602105252_RemoveEmployeeIdFromDevice.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CF.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEmployeeIdFromDevice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Device_DeviceType_DeviceTypeId",
+                table: "Device");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Device_Employee_EmployeeId",
+                table: "Device");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Device_EmployeeId",
+                table: "Device");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "Device");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeviceTypeId",
+                table: "Device",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Device_DeviceType_DeviceTypeId",
+                table: "Device",
+                column: "DeviceTypeId",
+                principalTable: "DeviceType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Device_DeviceType_DeviceTypeId",
+                table: "Device");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeviceTypeId",
+                table: "Device",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeId",
+                table: "Device",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Device_EmployeeId",
+                table: "Device",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Device_DeviceType_DeviceTypeId",
+                table: "Device",
+                column: "DeviceTypeId",
+                principalTable: "DeviceType",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Device_Employee_EmployeeId",
+                table: "Device",
+                column: "EmployeeId",
+                principalTable: "Employee",
+                principalColumn: "Id");
+        }
+    }
+}
